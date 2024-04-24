8f04d4a4320b4e4c3ed97aa17d888d041151861c
LLYS-Phys <lachyordanov@gmail.com>
2024-04-24 18:25:36 +0300

    add enter and enter+shift events

--- src/components/notes_logic.js
+++ src/components/notes_logic.js
@@ -10,23 +10,39 @@ window.addEventListener('load', () => {
         document.getElementById("noteinput").focus();
     })
 
-    document.getElementById("save").addEventListener("click", (event) => {
-        if (document.getElementById("noteinput").value.length > 1) {
-            let newNote = { id: lastId, text: document.getElementById("noteinput").value };
-            let li = document.createElement('li');
-            li.className = `note-${newNote.id}`;
-            deselectEls();
-            li.classList.add('selected');
-            li.innerHTML = newNote.text;
-            notelist.appendChild(li);
-            notes.push(newNote);
-            selectedNote = newNote;
-            isNewNote = false;
-            lastId++;
-            document.getElementById("noteinput").focus();
-            reset()
-        }
+    document.getElementById("save").addEventListener("click", () => {
+        save()
     });
+
+    document.getElementById("noteinput").addEventListener("keydown", (event) => {
+        if (event.keyCode == 13) {
+            if (event.shiftKey){
+                pasteIntoInput(this, "\n");
+            }
+            else{
+                save()
+                event.preventDefault()
+            }
+        }
+    })
+
+    function pasteIntoInput(el, text) {
+        if (el != undefined){
+            el.focus()
+            if (typeof el.selectionStart == "number"
+                    && typeof el.selectionEnd == "number") {
+                var val = el.value;
+                var selStart = el.selectionStart;
+                el.value = val.slice(0, selStart) + text + val.slice(el.selectionEnd);
+                el.selectionEnd = el.selectionStart = selStart + text.length;
+            } else if (typeof document.selection != "undefined") {
+                var textRange = document.selection.createRange();
+                textRange.text = text;
+                textRange.collapse(false);
+                textRange.select();
+            }
+        }
+    }
 
     document.getElementById("removenote").addEventListener("click", (event) => {
         if (selectedNote) {
@@ -81,6 +97,24 @@ window.addEventListener('load', () => {
             if (selectedElem){
                 selectedElem.classList.remove('selected');
             }
+        }
+    }
+
+    function save() {
+        if (document.getElementById("noteinput").value.length > 1) {
+            let newNote = { id: lastId, text: document.getElementById("noteinput").value };
+            let li = document.createElement('li');
+            li.className = `note-${newNote.id}`;
+            deselectEls();
+            li.classList.add('selected');
+            li.innerHTML = newNote.text;
+            notelist.appendChild(li);
+            notes.push(newNote);
+            selectedNote = newNote;
+            isNewNote = false;
+            lastId++;
+            document.getElementById("noteinput").focus();
+            reset()
         }
     }
 })
