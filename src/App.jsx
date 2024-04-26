140b00cc2711dbdc86651170083da6ddc74ca445
LLYS-Phys <lachyordanov@gmail.com>
2024-04-26 18:00:06 +0300

    clear unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Notes from './components/Notes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <BrowserRouter>
